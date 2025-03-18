aeac9da1f50bd0c20244e89f08240569716f0861
Nithin574 <nithins038@gmail.com>
2025-03-18 16:44:21 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -18,7 +18,7 @@ module tt_um_Nithin574 (
 
   // All output pins must be assigned. If not used, assign to 0.
    // Example: ou_out is the sum of ui_in and uio_in
-  assign uo_out = {7'b0000000, ui_out[0]}; 
+    assign uo_out = {7'b0000000, uo_out[0]}; 
   assign uio_out = 0;
   assign uio_oe  = 0;
 
