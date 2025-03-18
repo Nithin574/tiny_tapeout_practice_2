0851f37a5367f8ed199a46569f446bd39c419cd5
Nithin574 <nithins038@gmail.com>
2025-03-18 16:33:36 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -18,11 +18,12 @@ module tt_um_Nithin574 (
 
   // All output pins must be assigned. If not used, assign to 0.
    // Example: ou_out is the sum of ui_in and uio_in
+  assign ui_out[7:1] = 0;
   assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-    wire _unused = &{ena, uio_in[7:1], ui_in[7:1], ui_out[7:1], 1'b0};
+    wire _unused = &{ena, uio_in[7:1], ui_in[7:1], 1'b0};
 
     melay_11011_non_overlapping fsm(clk, rst_n, ui_in[0], ui_out[0]);
 
